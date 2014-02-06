c606a2805b36c88d3460dc4fcb490cfaec515cbc
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2014-02-06 16:47:06 -0500

    Merge branch 'master' of https://github.com/WebMole/crawler-benchmark
    added some kind of graph management at the same time
    
    Conflicts:
    	cb.py

--- GraphManager.py
+++ GraphManager.py
@@ -1,1 +1,22 @@
-__author__ = 'gableroux'
+import StringIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import random
+
+import LogParser
+
+def draw_custom_graph():
+
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+
+    requets = LogParser.get_log_dicts()
+    ys = range(0, len(requets))
+    xs = [request['datetime'] for request in requets]
+
+    axis.plot(xs, ys)
+
+    canvas = FigureCanvas(fig)
+    output = StringIO.StringIO()
+    canvas.print_png(output)
+    return output
