958d26806750e11cb5484620727701b0eb939cba
Nicolas Bergeron <nsb002@gmail.com>
2014-02-07 19:02:41 -0500

    -Results graphs now based on elapsed time from the first request. -BETA : Multiple lines on results graph by user_agent.

--- GraphManager.py
+++ GraphManager.py
@@ -1,79 +1,74 @@
-'''import StringIO
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-import random
-
-import LogParser
-
-def draw_custom_graph():
-
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-
-    requests = LogParser.get_log_dicts()
-    ys = range(0, len(requests))
-    xs = [request['datetime'] for request in requests]
-
-    axis.plot(xs, ys)
-
-    canvas = FigureCanvas(fig)
-    output = StringIO.StringIO()
-    canvas.print_png(output)
-    return output'''
-
 from __future__ import print_function
 import datetime
 import matplotlib.pyplot as plt
-from matplotlib.dates import HourLocator, MinuteLocator, DateFormatter
 
 import StringIO
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import LogParser
+from matplotlib import ticker
 
-# every minutes
-minutes   = MinuteLocator()
+import random
 
-# every hour
-hours     = HourLocator()
-hoursFmt  = DateFormatter("%H:%M:%S")
+def timeTicks(x, pos):
+    return "{:10.2f}".format(float(x) / 1.0)
+
+formatter = ticker.FuncFormatter(timeTicks)
+
+def get_total_seconds(td): return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 1e6) / 1e6
+
+def timeDelta(dt1, dt2):
+    #t1_ms = (dt1.hour*60*60 + dt1.minute*60 + dt1.second)*1000 + dt1.microsecond
+    #t2_ms = (dt2.hour*60*60 + dt2.minute*60 + dt2.second)*1000 + dt2.microsecond
+    #return max([t1_ms, t2_ms]) - min([t1_ms, t2_ms])
+    return get_total_seconds(max([dt1, dt2]) - min([dt1, dt2]))
 
 def draw_custom_graph():
-    #quotes = quotes_historical_yahoo('INTC', date1, date2)
-    requests = LogParser.get_log_dicts(user_agent = r'SiteSucker*.')
-    # todo: Eventually use the timedelta... Hell yeah!
-    #firstDate = None
-    #for request in requests:
-    #    if firstDate is None:
-    #        firstDate = request['datetime']
-    #    request['datetime'] = request['datetime'] - firstDate
+    requests = LogParser.get_log_dicts(user_agent = r'SiteSucker.*')
+    #requests = LogParser.get_log_dicts(user_agent = r'.*HTTrack.*')
 
-    #if len(quotes) == 0:
-    #    print ('Found no quotes')
-    #    raise SystemExit
+    firstDate = None
+    x = []
+    y = []
+    for index, request in enumerate(requests):
+        if index is not 0:
+            x.append(timeDelta(request['datetime'], firstDate))
+        else:
+            firstDate = request['datetime']
+            x.append(0)
+        y.append(index)
 
-    dates = [q['datetime'] for q in requests]
-    opens = range(0, len(requests)) #[q[1] for q in quotes]
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
 
-    fig, ax = plt.subplots()
-    ax.plot_date(dates, opens, '-', color=[1, 0, 0])
+    # TEST\
+    requests = LogParser.get_log_dicts(user_agent = r'.*HTTrack.*')
 
-    # Lambda Here to filter data...
+    firstDate = None
+    x2 = []
+    y2 = []
+    for index, request in enumerate(requests):
+        if index is not 0:
+            x2.append(timeDelta(request['datetime'], firstDate))
+        else:
+            firstDate = request['datetime']
+            x2.append(0)
+        y2.append(index)
 
-    ax.xaxis.set_major_locator(hours)
-    ax.xaxis.set_major_formatter(hoursFmt)
-    ax.xaxis.set_minor_locator(minutes)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    # TEST/
 
-    ax.autoscale_view()
-    #ax.xaxis.grid(False, 'major')
-    #ax.xaxis.grid(True, 'minor')
-    ax.grid(True)
+    # todo: Use different color (not only random) and add the ability to choose multiple user_agent.
+    ax.plot(x, y, color=random.choice(['r', 'b', 'g', 'c', 'm', 'y', 'k']))
+    # TEST\
+    ax.plot(x2, y2, color=random.choice(['r', 'b', 'g', 'c', 'm', 'y', 'k']))
+    # TEST/
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Request number')
 
-    fig.autofmt_xdate()
-
-    #plt.show()
+    ax.xaxis.set_major_formatter(formatter)
 
     output = StringIO.StringIO()
     canvas = FigureCanvas(fig)
-    #canvas.print_png(output)
     canvas.print_svg(output)
     return output
