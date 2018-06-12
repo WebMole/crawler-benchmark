b37fe198fb192c2bbbdc72c6a16ee72a20405361
benjamin.laprise <benjamin.laprise@yahoo.com>
2018-06-12 11:03:51 -0400

    Add a Postgres docker container for development

---
+++ wait-for-postgres.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# wait-for-postgres.sh
+
+set -e
+
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST"  -d"$POSTGRES_DB"  -U "$POSTGRES_USER" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping - $POSTGRES_PASSWORD - $POSTGRES_HOST"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command: $cmd"
+exec python runserver.py
